74e10378d6f05ba03595465e88c436c472777fb3
Dalton Tewanger <daltontewanger@yahoo.com>
2024-03-12 20:47:10 -0700

    Changed update route to integrate mongodb

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,71 +1,81 @@
-const router = require('express').Router()
-const Bread = require('../models/bread')
+const router = require("express").Router();
+const Bread = require("../models/bread");
 
 // GET retrieve all the bread
-router.get('/', async (req, res) => {
-    try{
-        const breads = await Bread.find()
-        res.render('index', {
-            breads
-        })
-    } catch (error) {
-        console.log('error:', error)
-        res.json({ message: 'error getting bread' })
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const breads = await Bread.find();
+    res.render("index", {
+      breads,
+    });
+  } catch (error) {
+    console.log("error:", error);
+    res.json({ message: "error getting bread" });
+  }
+});
 
 // Render New Page
-router.get('/new', (req, res) => {
-    res.render('new')
-})
+router.get("/new", (req, res) => {
+  res.render("new");
+});
 
 // GET retrieve bread by id
-router.get('/:id', async (req, res) => {
-    const { id } = req.params
-    const bread = await Bread.findById(id)
-    res.render('show', {
-        bread
-    })                                                                                                                             
-})
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const bread = await Bread.findById(id);
+  res.render("show", {
+    bread,
+  });
+});
 
-router.get('/:index/edit', (req, res) => {
-    const { index } = req.params
-    res.render('edit', {
-        bread:Bread[index],
-        index
-    })
-})
+router.get("/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const bread = await Bread.findById(id);
+    res.render("edit", {
+      bread,
+    });
+  } catch (error) {
+    console.log("error:", error);
+    res.json({ message: "error getting bread" });
+  }
+});
 
 // CREATE bread
-router.post('/', async (req, res) => {
-    if (!req.body.image) req.body.image = undefined
-    if (req.body.hasGluten === 'on') {
-        req.body.hasGluten = true
-    } else {
-        req.body.hasGluten = false
-    }
-    await Bread.create(req.body)
-    res.redirect('/bread')
-})
+router.post("/", async (req, res) => {
+  if (!req.body.image) req.body.image = undefined;
+  if (req.body.hasGluten === "on") {
+    req.body.hasGluten = true;
+  } else {
+    req.body.hasGluten = false;
+  }
+  await Bread.create(req.body);
+  res.redirect("/bread");
+});
 
 // PUT (UPDATE) Bread
-router.put('/:index', (req, res) => {
-    const { index } = req.params
-    if (!req.body.image) req.body.image = 'https://houseofnasheats.com/wp-content/uploads/2022/02/French-Bread-1.jpg'
-    if (req.body.hasGluten === 'on') {
-        req.body.hasGluten = true
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!req.body.image) req.body.image = undefined;
+    if (req.body.hasGluten === "on") {
+      req.body.hasGluten = true;
     } else {
-        req.body.hasGluten = false
+      req.body.hasGluten = false;
     }
-    Bread[index] = req.body
-    res.redirect(`/bread/${index}`)
-})
+    await Bread.findByIdAndUpdate(id, req.body, { new: true });
+    res.redirect(`/bread/${id}`);
+  } catch (error) {
+    console.log("Error:", error);
+    res.status(500).json({ message: "Error updating bread", error });
+  }
+});
 
 // DELETE Bread
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params
-    await Bread.findByIdAndDelete(id)
-    res.redirect('/bread')
-})
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  await Bread.findByIdAndDelete(id);
+  res.redirect("/bread");
+});
 
-module.exports = router
+module.exports = router;
